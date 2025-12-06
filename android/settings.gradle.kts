9bbf176ae76ffccac4f25c4027cce39c59a9581e
JOEL S RAPHAEL <joelraphael6425@gmail.com>
2025-12-06 11:59:24 +0530

    feat: Update Google Services configuration and adjust Gradle properties for compatibility

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,9 +18,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false // This is very recent, should be fine
-    id("com.google.gms.google-services") version "4.4.2" apply false // Updated
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false // <-- FIXED
+    id("com.android.application") version "8.7.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    // CHANGE THIS LINE BELOW FROM 2.0.0 TO 2.1.0
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false 
 }
-
 include(":app")
